e4a6e5f421c9e972b8befb809f75d26712ee2ebd
Murilosenai <murilo_ribeiro1@estudante.sc.senai.br>
2021-06-09 18:59:14 -0300

    Mostrando Alert

---
+++ javaFX_Model_MVC_2/src/application/Main.java
@@ -0,0 +1,48 @@
+package application;
+
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+
+/* Configuração do build:
+ * o Botão direito no projeto -> Run As -> Run Configurations -> Arguments -> VM Arguments
+ * o Copiar o conteúdo abaixo, adaptando para sua pasta:
+ * --module-path C:\java-libs\javafx-sdk\lib --add-modules=javafx.fxml,javafx.controls
+ */
+
+public class Main extends Application {
+	/*
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			BorderPane root = new BorderPane();
+			Scene scene = new Scene(root, 400, 400);
+			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	*/
+
+	@Override
+	public void start(Stage stage) {
+		try {
+			Parent parent = FXMLLoader.load(getClass().getResource("/gui/View.fxml"));
+			Scene scene = new Scene(parent);
+			stage.setScene(scene);
+			stage.show();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
